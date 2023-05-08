1d50544c073020e01cc83f12bb5fe187ca834f61
g-dailey <gulafroz.rezai@gmail.com>
2023-05-08 08:26:14 -0700

    Additional filtering operations practice

--- CoreApp/Program.cs
+++ CoreApp/Program.cs
@@ -155,27 +155,115 @@ namespace CoreApp
             //}
 
             //QUERY Syntax with GROUPJOIN and Order by 
-            var result = from dept in departmentlist
-                         join emp in employeelist
-                         on dept.Id equals emp.DepartmentId
-                         into employeeGroup
-                         select new
-                         {
-                             Employees = employeeGroup,
-                             DepartmentName = dept.LongName
-                         };
+            //var result = from dept in departmentlist
+            //             join emp in employeelist
+            //             on dept.Id equals emp.DepartmentId
+            //             into employeeGroup
+            //             select new
+            //             {
+            //                 Employees = employeeGroup,
+            //                 DepartmentName = dept.LongName
+            //             };
 
-            foreach (var item in result)
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($"Department Name: {item.DepartmentName}");
+            //    foreach (var emp in item.Employees)
+            //    {
+            //        Console.WriteLine($"\t{emp.FirstName} {emp.LastName}");
+            //    }
+
+            //}
+
+
+            ///// ALL, Any, Contains Quantifier Operator
+            //var annualSalaryCompare = 100000;
+
+            //bool isTrueAll = employeelist.All(e => e.AnnualSalary > annualSalaryCompare);
+
+            //if (isTrueAll)
+            //{
+            //    Console.WriteLine($"All employee annual salaries are above {annualSalaryCompare}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Not all employees's annual salary is above {annualSalaryCompare}");
+            //}
+
+
+            //bool isTrueAny = employeelist.Any(e => e.AnnualSalary > annualSalaryCompare);
+
+            //if (isTrueAny)
+            //{
+            //    Console.WriteLine($"Some of the employees annual salary is above {annualSalaryCompare}");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"None of the employee's annual salary is above {annualSalaryCompare}");
+            //}
+
+
+            ///// CONTAINS key work operator
+            //var searchEmployee = new Employee
+            //{
+            //    Id = 3,
+            //    FirstName = "Douglas",
+            //    LastName = "Robers",
+            //    AnnualSalary = 40000.2m,
+            //    IsManager = false,
+            //    DepartmentId = 1
+            //};
+            //bool containsEmployee = employeelist.Contains(searchEmployee, new EmployeeComparer());
+
+            //if (containsEmployee)
+            //{
+            //    Console.WriteLine($"An employee record for {searchEmployee.FirstName} {searchEmployee.LastName} was found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"An employee for {searchEmployee.FirstName} {searchEmployee.LastName} was not found");
+            //}
+
+
+
+            ///OfType Filter Operator
+            //ArrayList mixedCollection = Data.GetHeterogeneousDataCollection();
+
+            //var stringResult = from s in mixedCollection.OfType<string>()
+            //                   select s;
+
+            //foreach (var item in stringResult)
+            //    Console.WriteLine(item);
+
+
+            ///Element At or Element at by Default
+            //var emp = employeelist.ElementAtOrDefault(12);
+
+            //if (emp != null)
+            //{
+            //    Console.WriteLine($"{emp.FirstName,-10}{emp.LastName,-10}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This employee does not exist in this collection");
+            //}
+
+
+
+            /////
+            List<int> integerList = new List<int> { 3, 14, 23, 17, 27, 89 };
+
+            int result = integerList.FirstOrDefault(i => i % 2 == 0);
+            if (result != 0)
             {
-                Console.WriteLine($"Department Name: {item.DepartmentName}");
-                foreach (var emp in item.Employees)
-                {
-                    Console.WriteLine($"\t{emp.FirstName} {emp.LastName}");
-                }
 
+                Console.WriteLine(result);
             }
-
-
+            else
+            {
+                Console.WriteLine("There are no even numbers in the Collection");
+            }
 
             Console.ReadKey();
         }
@@ -195,6 +283,26 @@ namespace CoreApp
 
         }
     }
+
+    //Employee Comparer class
+
+    //public class EmployeeComparer : IEqualityComparer<Employee>
+    //{
+    //    public bool Equals([AllowNull] Employee x, [AllowNull] Employee y)
+    //    {
+    //        if (x.Id == y.Id && x.FirstName.ToLower() == y.FirstName.ToLower() && x.LastName.ToLower() == y.LastName.ToLower())
+    //        {
+    //            return true;
+    //        }
+    //        return false;
+    //    }
+    //    public int GetHashCode([DisallowNull] Employee obj)
+    //    {
+    //        return obj.Id.GetHashCode();
+    //    }
+    //}
+
+
 
     public class Employee
     {
@@ -286,6 +394,25 @@ namespace CoreApp
             return departments;
 
         }
+
+       //public static ArrayList GetHeterogeneousDataCollection()
+       // {
+       //     ArrayList arrayList = new ArrayList();
+       //     arrayList.Add(100);
+       //     arrayList.Add("Bob Jones");
+       //     arrayList.Add(2000);
+       //     arrayList.Add(3000);
+       //     arrayList.Add("Bill Henderson");
+       //     arrayList.Add(new Employee { Id = 6, FirstName = "Jennifer", LastName = "Dale", AnnualSalary = 90000, IsManager = false,
+       //     DepartmentId = 3});
+       //     arrayList.Add(new Employee { Id = 6, FirstName = "Dane", LastName = "Huges", AnnualSalary = 60000, IsManager = true,
+       //     DepartmentId = 4});
+       //     arrayList.Add(new Department { Id = 4, ShortName = "MKT", LongName = "Marketing"});
+       //     arrayList.Add(new Department { Id = 5, ShortName = "R&D", LongName = "Research and Development" });
+       //     arrayList.Add(new Department { Id = 6, ShortName = "PRD", LongName = "Production" });
+
+       //     return arrayList;
+       // }
 
     }
 }
