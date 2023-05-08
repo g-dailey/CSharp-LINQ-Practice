55079d6072a8d9435bdcbc6a401d3ba3f3f13f1c
g-dailey <gulafroz.rezai@gmail.com>
2023-05-08 09:28:49 -0700

    Finished practice of basic LINQ operators

--- CoreApp/Program.cs
+++ CoreApp/Program.cs
@@ -15,7 +15,7 @@ namespace CoreApp
 
             //Select and where operator - Method Syntax
 
-            //var result = employeelist.Select(e => new
+            //var employeelist = employeelist.Select(e => new
             //{
             //    FullName = e.FirstName + " " + e.LastName,
             //    AnnualSalary = e.AnnualSalary
@@ -252,36 +252,292 @@ namespace CoreApp
 
 
             /////
-            List<int> integerList = new List<int> { 3, 14, 23, 17, 27, 89 };
+            //List<int> integerList = new List<int> { 3, 14, 23, 17, 27, 89 };
 
-            int result = integerList.FirstOrDefault(i => i % 2 == 0);
-            if (result != 0)
-            {
+            //int result = integerList.FirstOrDefault(i => i % 2 == 0);
+            //if (result != 0)
+            //{
 
-                Console.WriteLine(result);
-            }
-            else
-            {
-                Console.WriteLine("There are no even numbers in the Collection");
-            }
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("There are no even numbers in the Collection");
+            //}
+
+            ///Single Operator
+            //var emp = employeelist.Single(e => e.Id == 2);
+            //Console.WriteLine($" {emp.Id,-5} {emp.FirstName,-10} {emp.LastName,-10}");
+
+
+            //Equality Operator
+            //var integerList1 = new List<int> { 1, 2, 3, 4, 5, 6 };
+            //var integerList2 = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+            //var boolSequenceEqual = integerList1.SequenceEqual(integerList2);
+            //Console.WriteLine(boolSequenceEqual);
+
+
+
+            ////Concat
+            //List<Employee> employeelist2 = new List<Employee> { new Employee
+            //{
+            //    Id = 5,
+            //    FirstName = "Tony",
+            //    LastName = "Garcia",
+            //    AnnualSalary = 90000,
+
+            //}, new Employee
+            //{
+            //    Id = 6,
+            //    FirstName = "Tony",
+            //    LastName = "Garcia",
+            //    AnnualSalary = 90000,
+
+            //}
+            //};
+            //IEnumerable<Employee> results = employeelist.Concat(employeelist2);
+
+            //foreach(var item in results)
+            //{
+            //    Console.WriteLine($"{item.FirstName,-10}");
+
+            //}
+
+
+            ///AGGREGAT
+            //decimal totalAnnualSalary = employeelist.Aggregate<Employee, decimal>(0, (totalAnnualSalary, e) =>
+            //{
+            //    var bonus = (e.IsManager) ? 0.04m : 0.02m;
+            //    totalAnnualSalary = (e.AnnualSalary + (e.AnnualSalary * bonus)) + totalAnnualSalary;
+            //    return totalAnnualSalary;
+            //});
+            //Console.WriteLine($"Total Annual Salary of all employees (including bonus): {totalAnnualSalary}");
+
+
+            ///COUNT
+            //int countEmployees = employeelist.Count(e => e.DepartmentId == 2);
+            //Console.WriteLine($"Number of Employee (Finance Department): {countEmployees}");
+
+            //SUM
+            //decimal result = employeelist.Sum(e => e.AnnualSalary);
+            //Console.WriteLine($"Total Annual Salary: {result}");
+
+            ////MAX
+            //var results = employeelist.Max(e => e.AnnualSalary);
+            //Console.WriteLine($"Max Salary: {results}");
+
+            ////Generation Operators - DefaultIfEmpty, Empty, Range, Repeat
+            //DefaultIfEmpty
+            //List<int> intList = new List<int>();
+            //var newList = intList.DefaultIfEmpty();
+
+            //Console.WriteLine(newList.ElementAt(0));
+
+            //List<Employee> employees = new List<Employee>();
+            //var newList = employees.DefaultIfEmpty(new Employee { Id = 0 });
+
+            //var result = newList.ElementAt(0);
+
+            //if (result.Id == 0)
+            //    Console.WriteLine($"The list is empty");
+
+            ////Empty
+            //List<Employee> emptyEmployeeList = Enumerable.Empty<Employee>().ToList();
+
+            //emptyEmployeeList.Add(new Employee { Id = 7, FirstName = "Dan", LastName = "Brown" });
+
+            //foreach (var item in emptyEmployeeList)
+            //    Console.WriteLine($"{item.FirstName} {item.LastName}");
+
+            ////Range
+            //var intCollection = Enumerable.Range(25, 20);
+            //foreach (var item in intCollection)
+            //    Console.WriteLine(item);
+
+            ////Repeat
+            //var strCollection = Enumerable.Repeat<string>("Placeholder", 10);
+            //foreach (var item in strCollection)
+            //    Console.WriteLine(item);
+
+            ////Set Operators - Distinct, Except, Intersect, Union
+            ////Distinct
+
+            //List<int> list = new List<int> { 1, 2, 1, 4, 6, 2, 6, 7, 8, 7, 7, 7 };
+            //var results = list.Distinct();
+            //foreach (var item in results)
+            //    Console.WriteLine(item);
+
+            ////Except
+
+            //IEnumerable<int> collection1 = new List<int> { 1, 2, 3, 4 };
+            //IEnumerable<int> collection2 = new List<int> { 3, 4, 5, 6 };
+
+            //var results = collection2.Except(collection1);
+            //foreach (var item in results)
+            //    Console.WriteLine(item);
+
+            //List<Employee> employeeList2 = new List<Employee>();
+
+            //employeeList2.Add(new Employee
+            //    {
+            //        Id = 1,
+            //        FirstName = "Bob",
+            //        LastName = "Jones",
+            //        AnnualSalary = 60000.3m,
+            //        IsManager = true,
+            //        DepartmentId = 2
+            //    }
+            //);
+            //employeeList2.Add(new Employee
+            //    {
+            //        Id = 3,
+            //        FirstName = "Douglas",
+            //        LastName = "Roberts",
+            //        AnnualSalary = 40000.2m,
+            //        IsManager = false,
+            //        DepartmentId = 1
+            //    }
+            //);
+            //employeeList2.Add(new Employee
+            //    {
+            //        Id = 5,
+            //        FirstName = "Craig",
+            //        LastName = "Laurence",
+            //        AnnualSalary = 20000.2m,
+            //        IsManager = false,
+            //        DepartmentId = 1
+            //    }
+            //);
+            //employeeList2.Add(new Employee
+            //    {
+            //        Id = 6,
+            //        FirstName = "Elizabeth",
+            //        LastName = "Tate",
+            //        AnnualSalary = 85000,
+            //        IsManager = true,
+            //        DepartmentId = 1
+            //    }
+            //);
+
+            //var results = employeeList.Except(employeeList2, new EmployeeComparer());
+
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10}");
+
+            ////Intersect
+            //var results = employeeList.Intersect(employeeList2, new EmployeeComparer());
+
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10}");
+
+            ////Union
+            //var results = employeeList.Union(employeeList2, new EmployeeComparer());
+
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10}");
+
+            ////Partitioning Operators - Skip, SkipWhile, Take, TakeWhile
+            ////Skip
+
+            //var results = employeeList.Skip(2);
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10}");
+
+            ////SkipWhile
+
+            //employeeList.Add(new Employee { Id = 5, FirstName = "Sam", LastName = "Davis", AnnualSalary = 100000.0m });
+
+            //var results = employeeList.SkipWhile(e => e.AnnualSalary > 50000);
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10} {item.AnnualSalary,10}");
+
+            ////Take
+            //var results = employeeList.Take(2);
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10}");
+
+            ////TakeWhile
+
+            //employeeList.Add(new Employee { Id = 5, FirstName = "Sam", LastName = "Davis", AnnualSalary = 100000 });
+
+            //var results = employeeList.TakeWhile(e => e.AnnualSalary > 50000);
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10} {item.AnnualSalary,10}");
+
+            ////Conversion Operators - ToList, ToDictionary, ToArray
+            ////ToList
+
+            //List<Employee> results = (from emp in employeeList
+            //                         where emp.AnnualSalary > 50000
+            //                         select emp).ToList();
+
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10} {item.AnnualSalary,10}");
+
+            ////ToDictionary
+
+            //Dictionary<int, Employee> dictionary = (from emp in employeeList
+            //                                        where emp.AnnualSalary > 50000
+            //                                        select emp).ToDictionary<Employee, int>(e => e.Id);
+
+            //foreach (var key in dictionary.Keys)
+            //    Console.WriteLine($"Key: {key}, Value: {dictionary[key].FirstName} {dictionary[key].LastName}");
+
+            ////ToArray
+            //Employee[] results = (from emp in employeeList
+            //                      where emp.AnnualSalary > 50000
+            //                      select emp).ToArray();
+
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10} {item.AnnualSalary,10}");
+
+            ////Let Clause and Into Clause
+
+            ////Let
+            //var results = from emp in employeeList
+            //              let Initials = emp.FirstName.Substring(0, 1).ToUpper() + emp.LastName.Substring(0, 1).ToUpper()
+            //              let AnnualSalaryPlusBonus = (emp.IsManager) ? emp.AnnualSalary + (emp.AnnualSalary * 0.04m) : emp.AnnualSalary + (emp.AnnualSalary * 0.02m)
+            //              where Initials == "JS" || Initials == "SJ" && AnnualSalaryPlusBonus > 50000
+            //              select new
+            //              {
+            //                  Initials = Initials,
+            //                  FullName = emp.FirstName + " " + emp.LastName,
+            //                  AnnualSalaryPlusBonus = AnnualSalaryPlusBonus
+            //              };
+
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Initials,-5} {item.FullName,-20} {item.AnnualSalaryPlusBonus,10}");
+
+            ////Into
+            //var results = from emp in employeeList
+            //              where emp.AnnualSalary > 50000
+            //              select emp
+            //              into HighEarners
+            //              where HighEarners.IsManager == true
+            //              select HighEarners;
+
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10} {item.AnnualSalary,10}\t{item.IsManager}");
+
+            ////Projection Operators - Select, SelectMany
+            ////Select
+            //var results = departmentList.Select(d => d.Employees);
+
+            //foreach(var items in results)
+            //    foreach(var item in items)
+            //        Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10} {item.AnnualSalary,10}\t{item.IsManager}");
+
+            ////SelectMany
+            //var results = departmentList.SelectMany(d => d.Employees); //.OrderBy(o => o.Id);
+
+            //foreach (var item in results)
+            //    Console.WriteLine($"{item.Id,-5} {item.FirstName,-10} {item.LastName,-10} {item.AnnualSalary,10}\t{item.IsManager}");
+
 
             Console.ReadKey();
         }
 
-    }
-
-    public static class EnumberableCollectionExtensionMethods
-    {
-        public static IEnumerable<Employee> GetHighSalariedEmployees(this IEnumerable<Employee> employees)
-        {
-            foreach (Employee emp in employees)
-            {
-                Console.WriteLine($"Accessing employee: {emp.FirstName + " " + emp.LastName}");
-                if (emp.AnnualSalary >= 50000)
-                    yield return emp;
-            }
-
-        }
     }
 
     //Employee Comparer class
@@ -301,6 +557,22 @@ namespace CoreApp
     //        return obj.Id.GetHashCode();
     //    }
     //}
+
+
+    public static class EnumberableCollectionExtensionMethods
+    {
+        public static IEnumerable<Employee> GetHighSalariedEmployees(this IEnumerable<Employee> employees)
+        {
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine($"Accessing employee: {emp.FirstName + " " + emp.LastName}");
+                if (emp.AnnualSalary >= 50000)
+                    yield return emp;
+            }
+
+        }
+    }
+
 
 
 
